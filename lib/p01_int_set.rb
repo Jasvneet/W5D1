57bae72e82e9482a24effdb07552c54ee385cb57
Your Name <Your Email>
2023-04-24 14:17:44 -0700

    insert method done

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,8 +1,22 @@
 class MaxIntSet
+
+  attr_reader :store
   def initialize(max)
+    @max = max 
+    @store = Array.new(@max)
   end
 
   def insert(num)
+    raise "Out of bounds" if num > @max || num < 0 
+    @store.each_with_index do |el, i|
+      if num == i 
+        @store[i]= true 
+      else 
+        @store[i]= false 
+      end 
+
+    end 
+
   end
 
   def remove(num)
