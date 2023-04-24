6ad13d49f706551bac971fff179274a1d63e7e50
Your Name <Your Email>
2023-04-24 16:57:17 -0700

    Hashing Problem Complete

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    new_s = ""
+    self.each_with_index do |ele, i|
+      new_s += ele.to_s + i.to_s
+    end 
+    new_s.to_i
+
   end
 end
 
 class String
   def hash
+    alphabet = ("a".."z").to_a
+    new_s = ""
+    self.chars.each_with_index do |char, i|
+      new_s += alphabet.index(char).to_s + i.to_s
+    end 
+    new_s.to_i
   end
 end
 
@@ -16,6 +28,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    subsets = []
+    self.each do |k,v|
+      subsets << k.to_s.to_i
+      subsets << v.to_i
+    end 
+    subsets.sort 
+    subsets.hash 
+
   end
 end
